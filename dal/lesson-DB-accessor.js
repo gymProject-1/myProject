cb92b468aa54bc28dfcbb1462e638ecb4e78c3bf
רות קציר <213398738@mbydomain>
2023-02-14 14:03:35 +0200

    tuesday morning

--- dal/lesson-DB-accessor.js
+++ dal/lesson-DB-accessor.js
@@ -19,7 +19,7 @@ class LessonsDataAccessor {
     getLessonName = async (_id) => {
         const lesson = await Lesson.findOne({ where: { id: _id }, attributes: ['name'] }); 
         var l= lesson.dataValues['name'];
-        console.log("inn              "+l);
+       // console.log("inn              "+l);
         return l;
     }
 
