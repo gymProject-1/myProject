9156e64a89bf5dadd3431e9925f2d81f9c87871c
gymProject-1 <36213398738@mby.co.il>
2023-02-28 21:08:20 +0200

    commite home

--- routes/secretaryRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const secretaryController = require('../controllers/secretaryController')
-
-router.route('/')
-    .post(secretaryController.createNewActivity)
-router.route('/:id')
-    .delete(secretaryController.deleteActivity)
-module.exports = router
